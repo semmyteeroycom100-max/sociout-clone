75c3647e5edf19b52f53bd1d538bdab4c0416be1
semmyteeroycom100-max <semmyteeroycom100@gmail.com>
2026-07-01 11:06:20 -0400

    fix: add AuthProvider and YouTubeProvider to main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ThemeProvider } from './context/ThemeContext';
 import { ToastProvider } from './context/ToastContext';
+import { AuthProvider } from './context/AuthContext';
+import { YouTubeProvider } from './context/YouTubeContext';
 import { preloadSound, playClickSound } from './utils/sound';
 import './index.css';
 
@@ -20,7 +22,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
       <ToastProvider>
-        <App />
+        <AuthProvider>
+          <YouTubeProvider>
+            <App />
+          </YouTubeProvider>
+        </AuthProvider>
       </ToastProvider>
     </ThemeProvider>
   </React.StrictMode>
